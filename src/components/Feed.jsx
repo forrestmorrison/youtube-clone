5c5dc66894ae78b990bcd1bb5ff484c8d6dd6468
forrestmorrison <forrestmorrison@gmail.com>
2022-12-07 17:01:03 -0600

    created Video component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -10,6 +11,12 @@ const Feed = () => {
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
           Copyright 2022 TheyTube
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          New <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+        <Videos />
       </Box>
     </Stack>
   )
