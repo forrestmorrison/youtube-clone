271547a2fd128beac995b3d838039e538f7a10d1
forrestmorrison <forrestmorrison@gmail.com>
2022-12-08 11:17:36 -0600

    added selectedCategory & setSelectedCategory

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,20 +1,31 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Sidebar, Videos } from './';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New')
+
+  useEffect(() =>{
+    fetchFromAPI(`search?pat=snippet&q=${selectedCategory}`)
+  }, [])
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
-        <Sidebar />
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
           Copyright 2022 TheyTube
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-          New <span style={{ color: '#FC1503' }}>videos</span>
+          {selectedCategory} <span style={{ color: '#FC1503' }}>videos</span>
         </Typography>
         <Videos videos={{}} />
       </Box>
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search'
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
     params: {
@@ -14,6 +14,6 @@ const options = {
 
 export const fetchFromAPI = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-        
+
     return data;
 }
